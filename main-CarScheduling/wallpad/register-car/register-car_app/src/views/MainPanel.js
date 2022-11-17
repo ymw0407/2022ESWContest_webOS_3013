27e4f72395211a71918d0d230a820ec007f53be0
bentshrimp <bent_shrimp@naver.com>
2022-11-17 22:16:04 +0900

    Feat : 우측 상단 닫힘 버튼 클릭시 앱 종료 후 홈으로 이동 구현

--- main-CarScheduling/wallpad/register-car/register-car_app/src/views/MainPanel.js
+++ main-CarScheduling/wallpad/register-car/register-car_app/src/views/MainPanel.js
@@ -12,61 +12,79 @@ import css from "./MainPanel.module.less";
 const bridge = new LS2Request(); // LS2 서비스 요청 인스턴스 생성
 //-------------------------------------------------------------------
 const MainPanel = ({ onClick, title, ...rest }) => {
-    //useState를 통해서 log를 관리한다.
-    const [registerLogs, setRegisterLogs] = useState(["aa"]);
-    const [generalLogs, setGeneralLogs] = useState(["aa"]);
-    //-----------------------------------------------
-    // 해당 페이지가 새로 켜질때마다 maininit의 과정을 거친다.
-    useEffect(() => {
-        mainInit();
-    }, []);
-    //-----------------------------------------------------------
-    // mongodb connect -> schema -> show -> setlogs의 과정을 거쳐 log를 새로고침해준다.
-    function mainInit() {
-        console.log("mainInit");
-        let lsRequest = {
-            service: "luna://com.registercar.app.service",
-            method: "mainInit",
-            parameters: {},
-            onSuccess: (msg) => {
-                console.log(msg);
-                setRegisterLogs(msg.carData.register);
-                setGeneralLogs(msg.carData.general);
-            },
-            onFailure: (msg) => {
-                console.log(msg);
-            },
-        };
-        bridge.send(lsRequest);
-    }
-    //-------------------------------------------------------------
-    return (
-        <Panel {...rest}>
-            <Header title={title} />
-            <div>
-                <Scroller>
-                    <Button
-                        className={css.button}
-                        onClick={onClick}
-                        backgroundOpacity="transparent"
-                    >
-                        Register
-                    </Button>
-                    <Logs>{registerLogs}</Logs>
-                </Scroller>
-            </div>
-            <div>
-                <Scroller>
-                    <Logs1>{generalLogs}</Logs1>
-                </Scroller>
-            </div>
-        </Panel>
-    );
+  //useState를 통해서 log를 관리한다.
+  const [registerLogs, setRegisterLogs] = useState(["임시 등록 차량"]);
+  const [generalLogs, setGeneralLogs] = useState(["일반 등록 차량"]);
+  //-----------------------------------------------
+  // 해당 페이지가 새로 켜질때마다 maininit의 과정을 거친다.
+  useEffect(() => {
+    mainInit();
+  }, []);
+  //-----------------------------------------------------------
+  // mongodb connect -> schema -> show -> setlogs의 과정을 거쳐 log를 새로고침해준다.
+  function mainInit() {
+    console.log("mainInit");
+    let lsRequest = {
+      service: "luna://com.registercar.app.service",
+      method: "mainInit",
+      parameters: {},
+      onSuccess: (msg) => {
+        console.log(msg);
+        setRegisterLogs(msg.carData.register);
+        setGeneralLogs(msg.carData.general);
+      },
+      onFailure: (msg) => {
+        console.log(msg);
+      },
+    };
+    bridge.send(lsRequest);
+  }
+  //-------------------------------------------------------------
+  function closeApp(app_id) {
+    var lsRequest = {
+      service: "luna://com.registercar.app.service",
+      method: "close",
+      parameters: {
+        app_id: app_id,
+        subscribe: true,
+      },
+      onSuccess: (msg) => {
+        console.log(msg.reply);
+      },
+      onFailure: (msg) => {
+        console.log(msg);
+      },
+    };
+    bridge.send(lsRequest);
+  }
+  return (
+    <Panel {...rest}>
+      <Header title={title} onClose={() => closeApp("com.registercar.app")} />
+      <div>
+        <Scroller>
+          <Button
+            className={css.button}
+            onClick={onClick}
+            backgroundOpacity="transparent"
+          >
+            Register
+          </Button>
+          <Logs>{registerLogs}</Logs>
+        </Scroller>
+      </div>
+      <div>
+        <Scroller>
+          <Logs1>{generalLogs}</Logs1>
+        </Scroller>
+      </div>
+    </Panel>
+  );
 };
 
 MainPanel.propTypes = {
-    onClick: PropTypes.func,
-    title: PropTypes.string,
+  closeApp: PropTypes.func,
+  onClick: PropTypes.func,
+  title: PropTypes.string,
 };
 
 export default MainPanel;
--- main-CarScheduling/wallpad/register-car/register-car_service/webos_service.js
+++ main-CarScheduling/wallpad/register-car/register-car_service/webos_service.js
@@ -10,120 +10,125 @@ var schema = undefined;
 var Car = undefined;
 // connect => schema => show => send to Enact => close
 service.register("mainInit", async function (message) {
-    // DB 연결
-    let connect = await mongo
-        .connectDB()
-        .then((result) => {
-            console.log("[mainInit] " + result);
-        })
-        .catch((error) => {
-            console.log("[mainInit] " + error);
-        });
-    // -------------------------------------
-    // 스키마 설정
-    if (Car == undefined) {
-        schema = await mongo.carSchema("cars").then((result) => {
-            Car = result;
-            console.log("[mainInit] " + result);
-        });
-    }
-    // -------------------------------------
-    // 데이터 가져오기
-    let showGeneralCar = await mongo.showGeneralCarData(Car).then((result) => {
-        generalCarArray = result;
-        console.log("[mainInit] " + generalCarArray);
+  // DB 연결
+  let connect = await mongo
+    .connectDB()
+    .then((result) => {
+      console.log("[mainInit] " + result);
+    })
+    .catch((error) => {
+      console.log("[mainInit] " + error);
     });
-    let showRegisterCar = await mongo
-        .showRegisterCarData(Car)
-        .then((result) => {
-            registerCarArray = result;
-            console.log("[mainInit] " + registerCarArray);
-        });
-    // -------------------------------------
-    // 데이터 UI에 전송
-    message.respond({
-        carData: { general: generalCarArray, register: registerCarArray },
+  // -------------------------------------
+  // 스키마 설정
+  if (Car == undefined) {
+    schema = await mongo.carSchema("cars").then((result) => {
+      Car = result;
+      console.log("[mainInit] " + result);
     });
-    // -------------------------------------
-    // DB 연결 끊기
-    let close = mongo
-        .closeDB()
-        .then((result) => console.log("[mainInit] " + result))
-        .catch((err) => console.log("[mainInit] " + err));
-    // -------------------------------------
+  }
+  // -------------------------------------
+  // 데이터 가져오기
+  let showGeneralCar = await mongo.showGeneralCarData(Car).then((result) => {
+    generalCarArray = result;
+    console.log("[mainInit] " + generalCarArray);
+  });
+  let showRegisterCar = await mongo.showRegisterCarData(Car).then((result) => {
+    registerCarArray = result;
+    console.log("[mainInit] " + registerCarArray);
+  });
+  // -------------------------------------
+  // 데이터 UI에 전송
+  message.respond({
+    carData: { general: generalCarArray, register: registerCarArray },
+  });
+  // -------------------------------------
+  // DB 연결 끊기
+  let close = mongo
+    .closeDB()
+    .then((result) => console.log("[mainInit] " + result))
+    .catch((err) => console.log("[mainInit] " + err));
+  // -------------------------------------
 });
 
 // connect => schema => put => close
 service.register("registerCar", async function (message) {
-    luna.init(service);
-    // DB 연결
-    let connect = await mongo
-        .connectDB()
-        .then((result) => {
-            console.log("[registerCar] " + result);
-        })
-        .catch((error) => {
-            console.log("[registerCar] " + error);
-        });
-    // -------------------------------------
-    // DB에 임시 등록 차량 등록
-    let register = await mongo
-        .createRegisterCar(
-            Car,
-            message.payload.put.carNumber,
-            message.payload.put.startAt,
-            message.payload.put.endAt
-        )
-        .then((result) => {
-            console.log("[registerCar] " + result);
-            luna.tts(
-                message.payload.put.carNumber + " 차량이 임시등록 되었습니다"
-            );
-            luna.toast(
-                message.payload.put.carNumber + " 차량이 임시등록 되었습니다"
-            );
-            console.log("[registerCar] Register Successed!");
-        })
-        .catch((error) => console.log("[registerCar] " + error));
-    // -------------------------------------
-    // DB 연결 끊기
-    let close = mongo
-        .closeDB()
-        .then((result) => console.log("[registerCar] " + result))
-        .catch((err) => console.log("[registerCar] " + err));
-    // -------------------------------------
+  luna.init(service);
+  // DB 연결
+  let connect = await mongo
+    .connectDB()
+    .then((result) => {
+      console.log("[registerCar] " + result);
+    })
+    .catch((error) => {
+      console.log("[registerCar] " + error);
+    });
+  // -------------------------------------
+  // DB에 임시 등록 차량 등록
+  let register = await mongo
+    .createRegisterCar(
+      Car,
+      message.payload.put.carNumber,
+      message.payload.put.startAt,
+      message.payload.put.endAt
+    )
+    .then((result) => {
+      console.log("[registerCar] " + result);
+      luna.tts(message.payload.put.carNumber + " 차량이 임시등록 되었습니다");
+      luna.toast(message.payload.put.carNumber + " 차량이 임시등록 되었습니다");
+      console.log("[registerCar] Register Successed!");
+    })
+    .catch((error) => console.log("[registerCar] " + error));
+  // -------------------------------------
+  // DB 연결 끊기
+  let close = mongo
+    .closeDB()
+    .then((result) => console.log("[registerCar] " + result))
+    .catch((err) => console.log("[registerCar] " + err));
+  // -------------------------------------
 });
 
 // connect => schema => delete => show => close
 service.register("deleteCarData", async function (message) {
-    // DB 연결
-    let connect = await mongo
-        .connectDB()
-        .then((result) => {
-            console.log("[deleteCarData] " + result);
-        })
-        .catch((error) => {
-            console.log("[deleteCarData] " + error);
-        });
-    // -------------------------------------
-    // DB에서 특정 차량 번호를 삭제
-    let remove = await mongo
-        .deleteCarData(Car, message.payload.carNumber)
-        .then((result) => {
-            console.log(result);
-            console.log(message.payload.carNumber + " : delete success");
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    let show = await mongo.showCarData(Car).then((result) => {
-        carArray = result;
+  // DB 연결
+  let connect = await mongo
+    .connectDB()
+    .then((result) => {
+      console.log("[deleteCarData] " + result);
+    })
+    .catch((error) => {
+      console.log("[deleteCarData] " + error);
     });
-    // -------------------------------------
-    // DB 연결 끊기
-    let close = mongo
-        .closeDB()
-        .then((result) => console.log("[deleteCarData] " + result))
-        .catch((err) => console.log("[deleteCarData] " + err));
-    // -------------------------------------
+  // -------------------------------------
+  // DB에서 특정 차량 번호를 삭제
+  let remove = await mongo
+    .deleteCarData(Car, message.payload.carNumber)
+    .then((result) => {
+      console.log(result);
+      console.log(message.payload.carNumber + " : delete success");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  let show = await mongo.showCarData(Car).then((result) => {
+    carArray = result;
+  });
+  // -------------------------------------
+  // DB 연결 끊기
+  let close = mongo
+    .closeDB()
+    .then((result) => console.log("[deleteCarData] " + result))
+    .catch((err) => console.log("[deleteCarData] " + err));
+  // -------------------------------------
+});
+
+// 홈 화면으로 돌아가기
+service.register("close", (msg) => {
+  luna.init(service);
+  console.log(msg.payload);
+  // 해당 앱 종료
+  luna.closeApp(msg.payload.app_id);
+  // bareapp 실행
+  luna.launchApp("com.webos.app.home");
+  msg.respond({ returnValue: true });
 });
