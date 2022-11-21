2e0d1dffab8d4d4974438848cc2d23c3dd9805ec
Yun Min Woo <yunminwo1211@kookmin.ac.kr>
2022-11-21 17:37:58 +0900

    Feat : exercise file added & Testing file added

---
+++ EC2/express_file_server/VideoUploadTest/video.py
@@ -0,0 +1,11 @@
+import os
+
+file_name = "input.mp4"
+# final = "input.mp4"
+# print(final)
+# os.system(f"mv {file_name} {final}")
+os.system(f"python3 upload.py --vid {file_name}")
+# os.system(f"rm -f {final}")
+
+
+    
