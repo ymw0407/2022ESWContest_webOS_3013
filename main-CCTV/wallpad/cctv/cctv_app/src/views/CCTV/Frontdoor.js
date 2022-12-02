ff3763ccbffe2605a8fc56bedf58ef92a89fe27d
seiyoon <sera4084@naver.com>
2022-12-02 20:53:36 +0900

    UI 수정

--- main-CCTV/wallpad/cctv/cctv_app/src/views/CCTV/Frontdoor.js
+++ main-CCTV/wallpad/cctv/cctv_app/src/views/CCTV/Frontdoor.js
@@ -1,13 +1,18 @@
 import React from "react";
 import './Cctv.css';
 import { Link } from "react-router-dom";
-var ip = "192.168.1.28";
 
 const Frontdoor = ({ children }) => {
   return (
     
+<<<<<<< Updated upstream
     <div className="Cctv">
       <div className="app-title">CCTV<img src={"/cctv.png"} style={{float:'right' ,width:'120px'}}/></div>
+=======
+    <div className="Cctv" >
+      <button className="button btnPush xbutton" style={{width: '90px', height:'90px', marginTop: '100px', marginRight: '100px', float:'right'}}><img src={require("./x.png")} style={{width:'50px', height:'50px'}} /></button>
+      <div className="app-title">CCTV</div>
+>>>>>>> Stashed changes
       <div className="tem">
         <div className="app-list">
           <ul>
@@ -16,9 +21,15 @@ const Frontdoor = ({ children }) => {
             <li><Link to='/parking'>주차장</Link></li>
           </ul>
         </div>
+<<<<<<< Updated upstream
         <div className="content">
           <img src={"http://" + ip + "/"} style={{margin:'auto', display:'block', backgroundColor:'white', width:'70%'}}/> 
         </div>
+=======
+      </div>
+      <div className="content">
+        <img src="http://192.168.1.28/" style={{margin:'auto', display:'block', backgroundColor:'white', width:'70%'}}/> 
+>>>>>>> Stashed changes
       </div>
   </div>
       
--- main-CCTV/wallpad/cctv/cctv_app/src/views/CCTV/Playground.js
+++ main-CCTV/wallpad/cctv/cctv_app/src/views/CCTV/Playground.js
@@ -1,13 +1,17 @@
 import React from "react";
 import './Cctv.css';
 import { Link } from "react-router-dom";
-var ip = "192.168.1.110";
 
 const Playground = ({ children }) => {
   return (
     
     <div className="Cctv">
+<<<<<<< Updated upstream
       <div className="app-title">CCTV<img src={"/cctv.png"} style={{float:'right' ,width:'120px'}}/></div>
+=======
+      <button className="button btnPush xbutton" style={{width: '90px', height:'90px', marginRight: '100px', float:'right'}}><img src={require("./x.png")} style={{width:'50px', height:'50px'}} /></button>
+      <div className="app-title">CCTV</div>
+>>>>>>> Stashed changes
       <div className="tem">
         <div className="app-list">
           <ul>
@@ -16,11 +20,17 @@ const Playground = ({ children }) => {
             <li><Link to='/parking'>주차장</Link></li>
           </ul>
         </div>
+<<<<<<< Updated upstream
         <div className="content">
           <img src={"http://" + ip + "/"} style={{margin:'auto', display:'block', backgroundColor:'white', width:'70%'}}/> 
         </div>
+=======
+>>>>>>> Stashed changes
       </div>
-  </div>
+      <div className="content">
+        <img src="http://192.168.1.110/" style={{margin:'auto', display:'block', backgroundColor:'white', width:'100%'}}/>  
+      </div>
+    </div>
       
   );
 };
